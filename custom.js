a5627261e2d43634ed3a92eebfb5904ccaa0fdcf
Didem Sahin <36928327+didemshin@users.noreply.github.com>
2020-10-22 12:50:26 +0300

    Son düzenleme ve yorum satırları eklendi.

--- custom.js
+++ custom.js
@@ -1,18 +1,18 @@
-var searchList = new Array();
+var searchList = new Array(); //Arama listesi tutmak için oluşturulan list
 
 var customJquery = {
-    apiService: function (searchValue, removeSearchList) {
-        customJquery.searchResult.removeSearchResult(removeSearchList);
+    apiService: function (searchValue, removeSearchList) { //Arama sonuçlarını servise göndermek için oluşturulan fonksiyon
+        customJquery.searchResult.removeSearchResult(removeSearchList); //Önceki aramada listelenen sonuçları temizlemek için olan fonksiyon çağırılıyor
 
         var url = "http://www.omdbapi.com/?s=" + searchValue + "&apikey=a028feb1";
         $.post(url, function (data) {
             $.each(data.Search, function (index, item) {
-                customJquery.searchResult.addSearchResult(item, ".cart-list", ".search-result", data.Search);
+                customJquery.searchResult.addSearchResult(item, ".cart-list", ".search-result", data.Search); //Servisten dönen sonuçlar listelemek için olan fonksiyon çağırılıyor
             });
         });
     },
 
-    displayContainer: function (List, removeElement) {
+    displayContainer: function (List, removeElement) { //Arama sonucu veya favoriler boş ise alanları gizlemek için oluşturulan fonksiyon
         var hasDisplayBlock = $(removeElement).hasClass("d-block");
         var hasDisplayNone = $(removeElement).hasClass("d-none");
         if (List.length == 0) {
@@ -28,78 +28,18 @@ var customJquery = {
         }
     },
 
-    favorites: {
-        removeFavorites: function () {
-            $(document).on("click", ".favorites-list i.removeFavorites", function () {
-                $(this).closest(".cart-item").remove();
-                var favoriteList = $("div.favorites-list .cart-item");
-
-                customJquery.displayContainer(favoriteList, ".favorites-list");
-            });
-        },
-        addFavorites: function () {
-            $(document).on("click", ".search-result i.addFavorites", function () {
-                $(this).removeClass("addFavorites");
-                var cloneElement = $(this).closest('.cart-item').clone(true);
-                console.log(cloneElement);
-                $(cloneElement).find("i").addClass("removeFavorites");
-                $(".favorites-cart-list").append(cloneElement);
-
-                var favoriteList = $("div.favorites-list .cart-item");
-                customJquery.displayContainer(favoriteList, ".favorites-list");
-            });
-        },
-    },
-
-    searchHistory: {
-        removeSearchHistory: function () {
-            $(document).on("click", ".searchHistoryJS button.searchHistoryItem i.close-button", function () {
-                var value = $(this).closest('button').text();
-                var index = searchList.indexOf(value);
-                if (index != -1){
-                    searchList.splice(index, 1);
-                }
-                $(this).parent().remove();
-            });
-        },
-
-        addSearchHistory: function (searchValue) {
-            var hasItem = searchList.indexOf(searchValue);
-
-            if(hasItem == -1){
-                if(searchList.length > 9){
-                    searchList.shift();
-                    $(".searchHistoryItem:first-child").remove();
-                }
-                searchList.unshift(searchValue);
-                $(".search-history-list").append('<button class="btn btn-sm btn-secondary mx-2 mb-2 searchHistoryItem" type="button">' +
-                    '<span class="searchValue">'+searchValue+'</span>' +
-                    '<i class="far fa-times-circle close-button ml-1"></i>' +
-                    '</button>');
-            }
-        },
-
-        searchHistory: function () {
-            $(document).on("click", "span.searchValue", function () {
-                var searchValue = $(this).text();
-                $(".search-input").val(searchValue);
-                $(".search-btn").click();
-            });
-        }
-    },
-
     searchResult: {
-        searchClickEvent: function () {
+        searchClickEvent: function () { //Arama yapılması için button click fonksiyonu
             $(".search-btn").click(function () {
                 var searchValue = $(".search-input").val();
                 var removeList = $("div.cart-list .cart-item");
 
-                customJquery.apiService(searchValue, removeList);
-                customJquery.searchHistory.addSearchHistory(searchValue);
+                customJquery.apiService(searchValue, removeList); //Aranmak istenilen değere göre servis için fonksiyon çağırılır
+                customJquery.searchHistory.addSearchHistory(searchValue); //Aranılan kelimenin arama geçmişine eklenmesi için fonksiyon çağırılır
             });
         },
 
-        searchKeyupEvent: function () {
+        searchKeyupEvent: function () { //Üç karakterden fazla olması durumunda arama yapılması için oluşturulan fonksiyon
             $(".search-input").keyup(function () {
                 var searchValue = $(".search-input").val();
                 if (searchValue.length > 2) {
@@ -118,7 +58,7 @@ var customJquery = {
             $(elementClass).append("<div class='col-24 col-lg-3 mb-5 cart-item'>" +
                 "<div class='card border-0 shadow'>" +
                 "<div class='cart-icon'>" +
-                "<i class='fa favorites-style addFavorites data-poster-url='" + item.Poster + "'></i>" +
+                "<i class='fa favorites-style addFavorites'></i>" +
                 "</div>" +
                 "<img class='card-img-top' src='" + item.Poster + "'/>" +
                 "<div class='card-body text-center'>" +
@@ -128,6 +68,66 @@ var customJquery = {
 
             customJquery.displayContainer(elementList, removeElement);
         },
+    },
+
+    favorites: {
+        removeFavorites: function () { //Favoriler listesinden çıkarılmak için oluşturulan fonksiyon
+            $(document).on("click", ".favorites-list i.removeFavorites", function () {
+                $(this).closest(".cart-item").remove();
+                var favoriteList = $("div.favorites-list .cart-item");
+
+                customJquery.displayContainer(favoriteList, ".favorites-list");
+            });
+        },
+        addFavorites: function () { //Favoriler listesine eklemek için oluşturulan fonksiyon
+            $(document).on("click", ".search-result i.addFavorites", function () {
+                $(this).removeClass("addFavorites");
+                var cloneElement = $(this).closest('.cart-item').clone(true);
+                console.log(cloneElement);
+                $(cloneElement).find("i").addClass("removeFavorites");
+                $(".favorites-cart-list").append(cloneElement);
+
+                var favoriteList = $("div.favorites-list .cart-item");
+                customJquery.displayContainer(favoriteList, ".favorites-list");
+            });
+        },
+    },
+
+    searchHistory: {
+        removeSearchHistory: function () { //Arama geçmişinde bulunan değerleri silmek için oluşturulan fonksiyon
+            $(document).on("click", ".searchHistoryJS button.searchHistoryItem i.close-button", function () {
+                var value = $(this).closest('button').text();
+                var index = searchList.indexOf(value);
+                if (index != -1){ //Silinmek istenilen değer diziden de siliniyor
+                    searchList.splice(index, 1);
+                }
+                $(this).parent().remove();
+            });
+        },
+
+        addSearchHistory: function (searchValue) { //Arama geçmişini eklemek için oluşturulan fonksiyon
+            var hasItem = searchList.indexOf(searchValue);
+
+            if(hasItem == -1){ //Aynı değeri tekrar eklememek için yapılan kontrol
+                if(searchList.length > 9){ //10 taneden fazla arama geçmişi var ise ilk aramayı silmek için yapılan kontrol
+                    searchList.shift();
+                    $(".searchHistoryItem:first-child").remove();
+                }
+                searchList.unshift(searchValue);
+                $(".search-history-list").append('<button class="btn btn-sm btn-secondary mx-2 mb-2 searchHistoryItem" type="button">' +
+                    '<span class="searchValue">'+searchValue+'</span>' +
+                    '<i class="far fa-times-circle close-button ml-1"></i>' +
+                    '</button>');
+            }
+        },
+
+        searchHistory: function () { //Arama geçmişinde bulunan bir değeri tekrar aramak için oluşturulan fonksiyon
+            $(document).on("click", "span.searchValue", function () {
+                var searchValue = $(this).text();
+                $(".search-input").val(searchValue);
+                $(".search-btn").click();
+            });
+        }
     },
 };
 
